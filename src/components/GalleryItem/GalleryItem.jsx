e10650e7390ec83e9ed32ffb019fcb1122be07ed
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-10 17:15:55 -0500

    like button created

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,29 +1,38 @@
 import { useState, useEffect } from 'react';
+import likeBtn from '../../like-btn.png'
 
 function GalleryItem({
-    listItem,
-    // renderInfo
+    listItem, onClickLike
+
 }) {
-// set a new useState that will load on render. If false show img, if true show description
-// both img and description will have onClick to change the state value and rerender the opposite
+    // set a new useState that will load on render. If false show img, if true show description
+    // both img and description will have onClick to change the state value and rerender the opposite
     let [imgShowing, setImgShowing] = useState(false)
 
     return (
-        <li>
-            {
-                imgShowing ?
-             <div onClick={() => setImgShowing(false)}>
-                 {listItem.description}
-             </div>
-             :
-             <img src={listItem.path} 
-             onClick={() => setImgShowing(true)}
-             />
+        <>
+            <li>
+                {
+                    imgShowing ?
+                        <div onClick={() => setImgShowing(false)}>
+                            {listItem.description}
+                        </div>
+                        :
+                        <img src={listItem.path}
+                            onClick={() => setImgShowing(true)}
+                        />
 
-            }
-            {/* conditional rendering, if true run img, else run description */}
+                }
 
-        </li>
+            </li>
+            {/* create a like button */}
+            <button
+                onClick={() => { onClickLike(listItem) }}
+            >
+                <img id='like-btn' src={likeBtn} alt="" />
+            </button>
+            <span id='like-count'>{listItem.likes}</span>
+        </>
     )
 }
 
