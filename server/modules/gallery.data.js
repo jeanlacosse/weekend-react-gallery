35135fe561f28afefdeab3f3c8b38b30d10bca43
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-11 11:51:43 -0500

    database created and works

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,10 @@
-const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/baloons.jpg', description: 'A baloon and a dog at a birthday party.', likes: 0 },
-    { id: 3, path: 'images/dandelion.jpg', description: 'Spring dandelions in Kenwood park.', likes: 0 },
-    { id: 4, path: 'images/mimosa.jpg', description: 'The wife sipping a mimosa on a Sunday morning.', likes: 0 },
-    { id: 5, path: 'images/plants.jpg', description: 'A collection of houseplants in the dining room.', likes: 0 },
-    { id: 6, path: 'images/tea.jpg', description: 'Pot of tea and pastries while studying.', likes: 0 },
-];
+// const galleryItems = [
+//     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+//     { id: 2, path: 'images/baloons.jpg', description: 'A baloon and a dog at a birthday party.', likes: 0 },
+//     { id: 3, path: 'images/dandelion.jpg', description: 'Spring dandelions in Kenwood park.', likes: 0 },
+//     { id: 4, path: 'images/mimosa.jpg', description: 'The wife sipping a mimosa on a Sunday morning.', likes: 0 },
+//     { id: 5, path: 'images/plants.jpg', description: 'A collection of houseplants in the dining room.', likes: 0 },
+//     { id: 6, path: 'images/tea.jpg', description: 'Pot of tea and pastries while studying.', likes: 0 },
+// ];
 
-module.exports = galleryItems;
+// module.exports = galleryItems;
